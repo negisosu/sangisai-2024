efdcb80ab5956ca6af61b7cb90478a4ad1401bbc
tatsu <m22020@g.metro-cit.ac.jp>
2024-10-21 08:59:46 +0900

    OtherImageの編集

--- app/components/exhibition/customs/OtherImage.tsx
+++ app/components/exhibition/customs/OtherImage.tsx
@@ -10,52 +10,76 @@ import styles from "@/app/css/MySwiper.module.css"
 
 export function OtherImage({ content } :{ content: any}){
 
-
-    const slideSettings = {
-        0: {
-            slidesPerView: 1.4,
-            spaceBetween: 10,
-        },
-        1024: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-        },
-    };
+    // const slideSettings = {
+    //     0: {
+    //         slidesPerView: 1.4,
+    //         spaceBetween: 10,
+    //     },
+    //     1024: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 10,
+    //     },
+    // };
 
     return(
-        <div className="md:my-10 my-5 md:mx-4 mx-2">
-            <Swiper
-            className={styles.MySwiper}
-            modules={[Navigation, Pagination, Autoplay]}
-            breakpoints={slideSettings} // slidesPerViewを指定
-            slidesPerView={"auto"} // ハイドレーションエラー対策
-            centeredSlides={true} // スライドを中央に配置
-            loop={true} // スライドをループさせる
-            speed={800} // スライドが切り替わる時の速度
-            autoplay={{
-                delay: 2500,
-                disableOnInteraction: false,
-            }} // スライド表示時間
-            navigation // ナビゲーション（左右の矢印）
-            pagination={{
-                clickable: true,
-            }} // ページネーション, クリックで対象のスライドに切り替わる
-            >
-                {
+        // <div className="md:my-10 my-5 md:mx-4 mx-2">
+        //     <Swiper
+        //     className={styles.MySwiper}
+        //     modules={[Navigation, Pagination, Autoplay]}
+        //     breakpoints={slideSettings} // slidesPerViewを指定
+        //     slidesPerView={"auto"} // ハイドレーションエラー対策
+        //     centeredSlides={true} // スライドを中央に配置
+        //     loop={true} // スライドをループさせる
+        //     speed={800} // スライドが切り替わる時の速度
+        //     autoplay={{
+        //         delay: 2500,
+        //         disableOnInteraction: false,
+        //     }} // スライド表示時間
+        //     navigation // ナビゲーション（左右の矢印）
+        //     pagination={{
+        //         clickable: true,
+        //     }} // ページネーション, クリックで対象のスライドに切り替わる
+        //     >
+        //         {
+        //             content.images.map((image: any) => {
+        //                 return <SwiperSlide key={content.images.indexOf(image)}>
+        //                     <Image
+        //                     src={`${image.url}?width=${image.width}&height=${image.height}`}
+        //                     width={900}
+        //                     height={600}
+        //                     alt=""
+        //                     className="w-full md:h-96 h-44 object-cover"
+        //                     unoptimized={true}//繰り返し表示なので最適化オフ
+        //                     />
+        //                 </SwiperSlide>
+        //             })
+        //         }
+        //     </Swiper>
+        //</div>
+
+        <div className=" grid grid-cols-2">
+            {
+                content.images.length > 1 ?
                     content.images.map((image: any) => {
-                        return <SwiperSlide key={content.images.indexOf(image)}>
-                            <Image
-                            src={`${image.url}?width=${image.width}&height=${image.height}`}
-                            width={900}
-                            height={600}
-                            alt=""
-                            className="w-full md:h-96 h-44 object-cover"
-                            unoptimized={true}//繰り返し表示なので最適化オフ
-                            />
-                        </SwiperSlide>
+                        return <Image
+                        key={content.images.indexOf(image)}
+                        src={image.url}
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        unoptimized
+                        className="w-full md:p-4 p-2 md:h-96 h-44 object-cover"
+                        />
                     })
-                }
-            </Swiper>
+                :   <Image
+                src={content.images[0].url}
+                alt=""
+                width={1000}
+                height={1000}
+                unoptimized
+                className="w-full"
+                />
+            }
         </div>
     )
 }
