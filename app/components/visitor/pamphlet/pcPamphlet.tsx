e069bb0c05f1c120b515e27e01700b873a637e9c
tatsu <m22020@g.metro-cit.ac.jp>
2024-10-25 14:38:09 +0900

    Pamphlet画像のいかれ解決

--- app/components/visitor/pamphlet/pcPamphlet.tsx
+++ app/components/visitor/pamphlet/pcPamphlet.tsx
@@ -6,13 +6,13 @@ export function PcPamphlet({ page } : { page: string}) {
     console.log(page)
 
     return(
-        <div className="md:block hidden">
+        <div className="md:block hidden w-full">
             <Image
             src={pcPamphletImages[Number(page) - 1]}
             alt=""
             width={1000}
             height={1000}
-            className="w-full"
+            className="w-full h-auto"
             />
         </div>
     )
